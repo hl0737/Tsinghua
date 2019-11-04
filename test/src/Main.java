a5a10e7d752ad939571abf2025b1f4ac4a90f17d
hl19 <hl19@mails.tsinghua.edu.cn>
2019-11-04 17:22:20 +0800

    add test module

---
+++ test/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] a = {
+                38,36,119,65,64,29,105,22,111,43
+        };
+        double result = 0;
+        for (double v : a) {
+            result += v;
+        }
+        System.out.println(result);
+
+        double[] b = {
+
+        };
+        result = 0;
+        for (double v : b) {
+            result += v;
+        }
+        System.out.println(result);
+
+        double[] c = {
+
+        };
+        result = 0;
+        for (double v : c) {
+            result += v;
+        }
+        System.out.println(result);
+
+        double[] d = {
+
+        };
+        result = 0;
+        for (double v : d) {
+            result += v;
+        }
+        System.out.println(result);
+    }
+}
